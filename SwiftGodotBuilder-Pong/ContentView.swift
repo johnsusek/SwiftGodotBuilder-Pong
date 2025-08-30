a146d952ac914723d771b6b6add8b84e68d80782
John Susek <john@johnsolo.net>
2025-08-29 21:31:48 -0500

    Initial Commit

---
+++ SwiftGodotBuilder-Pong/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftGodotBuilder-Pong
+//
+//  Created by John Susek on 8/29/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftGodotBuilder-Pong/SwiftGodotBuilder_PongApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftGodotBuilder_PongApp.swift
+//  SwiftGodotBuilder-Pong
+//
+//  Created by John Susek on 8/29/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftGodotBuilder_PongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
